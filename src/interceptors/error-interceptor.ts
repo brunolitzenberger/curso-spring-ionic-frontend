cd84a21bac0c3fc078921d0bf7bdecef0c192564
litzenberger.bruno@gmail.com <bruno_l@microton.com.br>
2021-02-18 14:38:25 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,12 +1,15 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { AlertController } from "ionic-angular";
 import { Observable } from "rxjs";
 import { StorageService } from "../services/storage.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
 
-    constructor(public storage: StorageService){}
+    constructor(
+        public storage: StorageService,
+        public alertCtrl: AlertController,){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
      
@@ -21,9 +24,15 @@ export class ErrorInterceptor implements HttpInterceptor{
             }
 
             switch(errorObj.status){
+                case 401:
+                    this.handle401();
+                    break;
+
                 case 403:
                     this.handle403();
                     break;
+                default:
+                    this.handleDefaultError(errorObj);
             }
             
             return Observable.throw(errorObj);
@@ -34,6 +43,30 @@ export class ErrorInterceptor implements HttpInterceptor{
         this.storage.setLocalUser(null);
     }
 
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: Falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [{
+                text: 'Ok'
+            }]
+        })
+        alert.present();
+    }
+
+    handleDefaultError(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [{
+                text: 'Ok'
+            }]
+        })
+        alert.present();
+    }
+    
 };
 
 export const ErrorInterceptorProvider = {
