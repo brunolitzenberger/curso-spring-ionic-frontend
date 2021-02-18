322c9d1f276e66ee80ba8af76078a987f5a49991
litzenberger.bruno@gmail.com <bruno_l@microton.com.br>
2021-02-18 14:24:50 -0300

    Interceptor para incluir token nas requisições

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -16,10 +16,7 @@ export class ErrorInterceptor implements HttpInterceptor{
             if(!errorObj.status){
                 errorObj = JSON.parse(errorObj);
             }
-
-            console.log("Erro detectado pelo interceptor:")
-            console.log(errorObj);
-
+            
             return Observable.throw(errorObj);
         }) as any;
     } 
